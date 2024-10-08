adadd74d689be4e431691524cb62346461ad4cf7
Nisarg Shah <141799170+NisargRhino@users.noreply.github.com>
2024-10-08 15:55:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 
     async function fetch2DStructure(smiles, imgElement) {
         try {
-            const response = await fetch('http://127.0.0.1:5000/get_2d_structure', {
+            const response = await fetch('https://frag-predict-portal-2.onrender.com/get_2d_structure', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -29,7 +29,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 
     async function fetch3DStructure(smiles, viewerElement, downloadElement, filename) {
         try {
-            const response = await fetch('http://127.0.0.1:5000/get_3d_structure', {
+            const response = await fetch('https://frag-predict-portal-2.onrender.com/get_3d_structure', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -51,7 +51,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
             downloadElement.style.display = 'block';
             downloadElement.onclick = async function() {
                 try {
-                    const response = await fetch('http://127.0.0.1:5000/download_pdb', {
+                    const response = await fetch('https://frag-predict-portal-2.onrender.com/download_pdb', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
@@ -76,7 +76,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 
     async function predictFragment(smiles, suffix) {
         try {
-            const response = await fetch('http://127.0.0.1:5000/predict_fragment', {
+            const response = await fetch('https://frag-predict-portal-2.onrender.com/predict_fragment', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -110,7 +110,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 
     async function fetchScore(smiles) {
         try {
-            const response = await fetch('http://127.0.0.1:5000/score', {
+            const response = await fetch('https://frag-predict-portal-2.onrender.com/score', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -128,7 +128,7 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
     async function combineFragments(smiles1, smiles2) {
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/combine', {
+            const response = await fetch('https://frag-predict-portal-2.onrender.com/combine', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
