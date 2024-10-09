477b1f7d26bb4d550fcb641fc1ea1dc4150cf079
Nisarg Shah <141799170+NisargRhino@users.noreply.github.com>
2024-10-09 00:46:28 -0700

    Update docking.py

--- docking.py
+++ docking.py
@@ -62,6 +62,6 @@ def run_docking(smiles):
 
 if __name__ == "__main__":
     # Define the SMILES string to test
-    smiles = "C1C(C(C(C(C1N)OC2C(C(C(C(O2)CN)O)O)O)O)OC3C(C(C(C(O3)CO)O)N)O)N"  # Example SMILES string for kanamycin
-    score = run_docking(smiles)
-    print(f"Best docking score: {score}")
+    #smiles = "C1C(C(C(C(C1N)OC2C(C(C(C(O2)CN)O)O)O)O)OC3C(C(C(C(O3)CO)O)N)O)N"  # Example SMILES string for kanamycin
+    #score = run_docking(smiles)
+    #print(f"Best docking score: {score}")
